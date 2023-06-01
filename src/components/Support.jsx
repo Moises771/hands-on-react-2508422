38387ad4613601ecf202e5a80d809382cd6259b0
Moises771 <julianbrb23@gmail.com>
2023-06-01 14:18:02 +0200

    added useEffect

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -1,7 +1,15 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+const pageTile = document.title;
 
 export default () => {
   const [count, setCount] = useState(0)
+
+  useEffect(()=>{
+    if (count > 0) {
+      document.title = `${pageTile}--${count}`
+    }
+  }
+  )
   return (
     <button className="outline" onClick={() => setCount(count + 1)}>
       {count === 0 ? "Click to support": `Supported ${count} times`}
